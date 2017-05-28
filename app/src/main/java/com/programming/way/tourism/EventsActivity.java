08a27b776b5f461c5f2d59fd65ed9935aaee5901
Morsy Mohamed Elsokary <morsysoker@hotmail.com>
2017-05-28 03:27:55 +0200

    asda

--- app/src/main/java/com/programming/way/tourism/EventsActivity.java
+++ app/src/main/java/com/programming/way/tourism/EventsActivity.java
@@ -2,6 +2,19 @@ package com.programming.way.tourism;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class EventsActivity extends AppCompatActivity {
 
@@ -9,5 +22,65 @@ public class EventsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
+        // Building the Test Drawer
+        new DrawerBuilder().withActivity(this).build();
+
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_home);
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_item_settings);
+
+//create the drawer and remember the `Drawer` result object
+        Toolbar toolbar;
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        return true;
+                    }
+                })
+                .build();
+
+
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.mipmap.banner)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Morsy Mohamed").withEmail("Morsysoker@hotmail.com").withIcon(R.mipmap.user)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return true;
+                    }
+                })
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Hesham Morsy").withEmail("HeshamMorsy@hotmail.com").withIcon(R.mipmap.man)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return true;
+                    }
+                })
+                .build();
+
+//Now create your drawer and pass the AccountHeader.Result
+        new DrawerBuilder()
+                .withAccountHeader(headerResult)
+                //additional Drawer setup as shown above
+                .addDrawerItems(item1).addDrawerItems(item2).withActivity(this)
+                .build();
+
     }
+
 }
