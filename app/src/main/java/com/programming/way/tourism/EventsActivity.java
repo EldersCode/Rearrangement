594f06869b0b131719af74a2541d00d6a21c6bd6
Morsy Mohamed Elsokary <morsysoker@hotmail.com>
2017-05-31 23:33:01 +0200

    asda

--- app/src/main/java/com/programming/way/tourism/EventsActivity.java
+++ app/src/main/java/com/programming/way/tourism/EventsActivity.java
@@ -1,12 +1,14 @@
 package com.programming.way.tourism;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 
 import com.mikepenz.iconics.typeface.IIcon;
@@ -35,44 +37,53 @@ public class EventsActivity extends AppCompatActivity {
         new DrawerBuilder().withActivity(this).build();
 
         //if you want to update the items at a later time it is recommended to keep it in a variable
-        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_home);
-        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_item_settings);
-        SecondaryDrawerItem items3 = new SecondaryDrawerItem().withIdentifier(3).withName("hi");
+        final PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_home);
+        final SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_item_settings);
+        final SecondaryDrawerItem items3 = new SecondaryDrawerItem().withIdentifier(3).withName("hi");
 
 
-        ProfileDrawerItem userInformation = new ProfileDrawerItem();
-        ProfileDrawerItem RentingCompanyOfTheUser = new ProfileDrawerItem();
+        final ProfileDrawerItem userInformation = new ProfileDrawerItem();
+        final ProfileDrawerItem RentingCompanyOfTheUser = new ProfileDrawerItem();
 
-        AccountHeader headerResult = new AccountHeaderBuilder()
-                .withActivity(this)
-                .withHeaderBackground(R.mipmap.banner)
+
+//create the drawer and remember the `Drawer` result object
+        AccountHeader headerResult = new AccountHeaderBuilder().withActivity(this).withHeaderBackground(R.mipmap.banner)
                 .addProfiles(
-                        userInformation.withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon(R.mipmap.user),
-                        RentingCompanyOfTheUser.withName("Morsy mohamed morsy").withEmail("morsysoker@hotmail.com").withIcon(R.mipmap.man)
+                        new ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon(R.mipmap.user)
                 )
-
                 .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
                     @Override
                     public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                }).build();
 
-                        // close the drawar after header selection (return false to close)
+        Drawer result = new DrawerBuilder()
+
+                .withActivity(this)
+
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                         return true;
                     }
                 })
-
+                .withInnerShadow(true)
+                .withSliderBackgroundColor(Color.argb(3, 105, 58, 42))
+                .withAccountHeader(headerResult,true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withActionBarDrawerToggle(true)
+                .withHeaderDivider(true)
+                .withHeaderPadding(true)
+                .withFooter(R.layout.activity_new_apartment_information)
+                .withFooterDivider(true)
                 .build();
-
-//Now create your drawer and pass the AccountHeader.Result
-        new DrawerBuilder().withAccountHeader(headerResult).addDrawerItems(item2).withActivity(this)
-                //additional Drawer setup as shown above
-                .build();
-       userInformation.withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-           @Override
-           public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-
-               return false;
-           }
-       });
 
     }
 
