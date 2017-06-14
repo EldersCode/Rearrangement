30c67d85e5bbd3f00b158de9bc2621442dc298e1
HeshamMorsy <Hesham0hero@gmail.com>
2017-06-14 02:01:14 +0200

    sba7 elfol tani

--- app/src/main/java/com/programming/way/tourism/MapsActivity.java
+++ app/src/main/java/com/programming/way/tourism/MapsActivity.java
@@ -212,12 +212,4 @@ public class MapsActivity extends HandlingMaps {
     }
     }
 
-
-
-    public Bitmap resizeMapIcons(String iconName, int width, int height){
-        Bitmap imageBitmap = BitmapFactory.decodeResource(getResources(),getResources().getIdentifier(iconName, "drawable", getPackageName()));
-        Bitmap resizedBitmap = Bitmap.createScaledBitmap(imageBitmap, width, height, false);
-        return resizedBitmap;
-    }
-
 }
